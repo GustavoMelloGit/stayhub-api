9d21450c6e79dfdb2c0aae19a540366a873e2a7a
Gustavo Marques <gugamello2014@yahoo.com>
2025-08-13 08:25:38 -0300

    feat: introduce HttpControllerMethod enum

--- src/presentation/controller/controller.ts
+++ src/presentation/controller/controller.ts
@@ -1,13 +1,21 @@
+export enum HttpControllerMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+}
+
 export type ControllerRequest = {
   params: Record<string, string>;
   body: Record<string, unknown>;
   query: Record<string, string>;
   headers: Record<string, string>;
-  method: string;
+  method: HttpControllerMethod;
   url: string;
 };
 
 export interface Controller {
   path: string;
+  method: HttpControllerMethod;
   handle(request: ControllerRequest): Promise<Response>;
 }
