fb3716a9140a2b554f8cc9127639e78ca7480875
Gustavo Marques <gugamello2014@yahoo.com>
2025-08-13 13:41:15 -0300

    feat: add create tenant and stay controllers, update routing logic to handle multiple methods

--- src/infra/http/routes/routes.ts
+++ src/infra/http/routes/routes.ts
@@ -6,13 +6,30 @@ const tenantDi = new TenantDi();
 const stayDi = new StayDi();
 
 const listTenantsController = tenantDi.makeListTenantsController();
+const createTenantController = tenantDi.makeCreateTenantController();
 const getStayController = stayDi.makeGetStayController();
+const createStayController = stayDi.makeCreateStayController();
 
-const controllers = [listTenantsController, getStayController];
+const controllers = [
+  listTenantsController,
+  createTenantController,
+  getStayController,
+  createStayController,
+];
 
-export const bunRoutes = controllers.reduce<
-  Record<string, (req: Request) => Promise<Response>>
->((acc, controller) => {
-  acc[controller.path] = BunHttpControllerAdapter(controller);
-  return acc;
-}, {});
+const routeMap = new Map();
+
+controllers.forEach((controller) => {
+  const alreadyExists = routeMap.get(controller.path);
+  if (!alreadyExists) {
+    routeMap.set(controller.path, {
+      [controller.method]: BunHttpControllerAdapter(controller),
+    });
+  }
+  routeMap.set(controller.path, {
+    ...alreadyExists,
+    [controller.method]: BunHttpControllerAdapter(controller),
+  });
+});
+
+export const bunRoutes = Object.fromEntries(routeMap.entries());
