abeb085c8cd33afc4c634b04820812192b4f45d7
Gustavo Marques <gugamello2014@yahoo.com>
2025-08-14 14:54:02 -0300

    feat: implement database connection verification in main function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
+import { sql } from "drizzle-orm";
 import { env } from "./infra/config/environments";
+import { db } from "./infra/database/drizzle/database";
 import { bunRoutes } from "./infra/http/routes/routes";
 
-function main() {
+async function checkDatabaseConnection() {
+  try {
+    await db.execute(sql`SELECT 1`);
+    console.log(
+      "✅ Connection to the database has been successfully verified.",
+    );
+  } catch (error) {
+    console.error("❌ Unable to connect to the database:", error);
+    process.exit(1);
+  }
+}
+
+async function main() {
+  await checkDatabaseConnection();
+
   const server = Bun.serve({
     port: env.PORT,
     routes: bunRoutes,
   });
 
-  console.log(`Listening on http://localhost:${server.port} ...`);
+  console.log(`🚀 Listening on http://localhost:${server.port} ...`);
 }
 
 main();
