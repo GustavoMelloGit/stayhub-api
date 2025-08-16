091df02cab0977ec8c42a09746b0adddceb8fb6c
Gustavo Marques <gugamello2014@yahoo.com>
2025-08-16 19:11:50 -0300

    feat: create default calendar for user on register

--- src/application/use_case/auth/register_user.ts
+++ src/application/use_case/auth/register_user.ts
@@ -1,5 +1,7 @@
+import { Calendar } from "../../../domain/entity/calendar";
 import { User } from "../../../domain/entity/user";
 import type { AuthRepository } from "../../../domain/repository/auth_repository";
+import type { CalendarRepository } from "../../../domain/repository/calendar_repository";
 import { ConflictError } from "../../error/conflict_error";
 import type { Hasher } from "../../service/hasher";
 import type { UseCase } from "../use_case";
@@ -20,6 +22,7 @@ export class RegisterUserUseCase implements UseCase<Input, Output> {
   constructor(
     private readonly userRepository: AuthRepository,
     private readonly hasher: Hasher,
+    private readonly calendarRepository: CalendarRepository,
   ) {}
 
   async execute(input: Input): Promise<Output> {
@@ -32,7 +35,15 @@ export class RegisterUserUseCase implements UseCase<Input, Output> {
     const hashedPassword = await this.hasher.hash(input.password);
 
     const user = User.create({ ...input, password: hashedPassword });
+
     const savedUser = await this.userRepository.addUser(user);
+
+    const calendar = Calendar.create({
+      name: `${input.name}'s Calendar`,
+      user_id: savedUser.id,
+    });
+
+    await this.calendarRepository.save(calendar);
 
     return {
       id: savedUser.id,
