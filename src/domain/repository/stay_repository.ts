c6a8eaca45cb78b0365233478252d791fb617e16
Gustavo Marques <gugamello2014@yahoo.com>
2025-08-16 19:01:22 -0300

    refactor: update StayRepository to use Stay entity directly and remove SaveStayDto

--- src/domain/repository/stay_repository.ts
+++ src/domain/repository/stay_repository.ts
@@ -2,16 +2,7 @@ import type { Stay } from "../entity/stay";
 import type { WithTenant } from "../entity/tenant";
 
 export interface StayRepository {
-  save(input: Omit<SaveStayDto, "id">): Promise<SaveStayDto>;
+  save(input: Stay): Promise<Stay>;
   findById(id: string): Promise<Stay | null>;
   findWithTenantById(id: string): Promise<WithTenant<Stay> | null>;
 }
-
-export type SaveStayDto = {
-  id: string;
-  tenant_id: string;
-  check_in: Date;
-  check_out: Date;
-  guests: number;
-  password: string;
-};
--- src/infra/database/postgres_repository/stay_postgres_repository.ts
+++ src/infra/database/postgres_repository/stay_postgres_repository.ts
@@ -1,10 +1,7 @@
 import { eq } from "drizzle-orm";
 import { Stay } from "../../../domain/entity/stay";
 import { Tenant, type WithTenant } from "../../../domain/entity/tenant";
-import type {
-  SaveStayDto,
-  StayRepository,
-} from "../../../domain/repository/stay_repository";
+import type { StayRepository } from "../../../domain/repository/stay_repository";
 import { db } from "../drizzle/database";
 import { staysTable } from "../drizzle/schema";
 
@@ -27,23 +24,13 @@ export class StayPostgresRepository implements StayRepository {
     return stayEntity as WithTenant<Stay>;
   }
 
-  async save(input: Omit<SaveStayDto, "id">): Promise<SaveStayDto> {
-    const entity = Stay.create(input);
+  async save(input: Stay): Promise<Stay> {
+    const result = await db.insert(staysTable).values(input.data).returning();
 
-    const stay = await db
-      .insert(staysTable)
-      .values({
-        check_in: entity.check_in,
-        check_out: entity.check_out,
-        guests: entity.guests,
-        password: entity.password,
-        tenant_id: entity.tenant_id,
-      })
-      .returning();
+    const stay = result[0];
+    if (!stay) throw new Error("Failed to save stay");
 
-    if (!stay[0]) throw new Error("Failed to save stay");
-
-    return stay[0];
+    return Stay.reconstitute(stay);
   }
 
   async findById(id: string): Promise<Stay | null> {
