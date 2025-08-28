0429d005f66079a0156ce07800d04b457ce82a57
Gustavo Marques <gugamello2014@yahoo.com>
2025-08-28 20:57:26 -0300

    chore: add redis to docker compose and add variables to env

--- src/infra/config/environments.ts
+++ src/infra/config/environments.ts
@@ -7,6 +7,10 @@ const envSchema = z.object({
     .enum(["development", "sandbox", "production"])
     .default("development"),
   JWT_SECRET: z.string(),
+  REDIS_USERNAME: z.string().default("default"),
+  REDIS_HOST: z.string().default("localhost"),
+  REDIS_PORT: z.coerce.number().default(6379),
+  REDIS_PASSWORD: z.string().optional(),
 });
 
 export const env = envSchema.parse(process.env);
