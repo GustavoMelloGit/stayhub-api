226767c1a41595bcc1ec4ce91a0e5e3ccb0383c0
Gustavo Marques <gugamello2014@yahoo.com>
2025-10-02 19:12:25 -0300

    refactor: use static constant for event name in StayPaymentConfirmedEvent

--- src/booking/domain/event/stay_payment_confirmed_event.ts
+++ src/booking/domain/event/stay_payment_confirmed_event.ts
@@ -1,6 +1,7 @@
 import type { DomainEvent } from "../../../core/domain/event/domain_event";
 
 export class StayPaymentConfirmedEvent implements DomainEvent {
+  static readonly NAME = "stay_payment_confirmed";
   public readonly name: string;
   public readonly occurred_at: Date;
 
@@ -9,7 +10,7 @@ export class StayPaymentConfirmedEvent implements DomainEvent {
     public readonly property_id: string,
     public readonly paid_amount: number,
   ) {
-    this.name = "stay_payment_confirmed";
+    this.name = StayPaymentConfirmedEvent.NAME;
     this.occurred_at = new Date();
   }
 }
