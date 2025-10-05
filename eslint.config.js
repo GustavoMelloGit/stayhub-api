13331ca0c56a6c2b2b5abf9466abc0ad9a767fc8
Gustavo Marques <gugamello2014@yahoo.com>
2025-10-05 16:36:53 -0300

    chore: update configuration files for Prettier and ESLint, enhance linting scripts in package.json

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,13 @@ export default defineConfig([
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.node },
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": "error",
+      "prefer-const": "error",
+      "no-var": "error",
+    },
   },
   eslintConfigPrettier,
-  tseslint.configs.recommended,
+  ...tseslint.configs.recommended,
 ]);
